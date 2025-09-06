24f0983eb9a5f320c593e1401cc9912edf832d87
NItin-36703 <nitin616pal@gmail.com>
2025-09-06 17:01:02 +0000

    mathsandnum

---
+++ 01_basics/numsandmaths.js
@@ -0,0 +1,31 @@
+let score = 20979
+console.log(score)
+console.log(typeof(score))
+let balance = new Number(23.4567) 
+console.log(balance);
+//  balance = 289357
+//   console.log(balance)
+console.log(balance.toFixed(2))
+console.log(balance.toPrecision(3))
+
+const n= 10000000
+console.log(n.toLocaleString('en-IN'))
+
+
+console .log ("now we begin maths!!!!!")
+
+///// lets begin maths 
+
+console.log (Math);
+console.log(Math.abs(-4));
+console.log(Math.round(9.5));
+console.log(Math.ceil(4.4));
+
+
+console.log(Math.random());
+
+
+
+
+
+
