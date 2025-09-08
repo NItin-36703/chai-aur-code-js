1bf8cb22719db32a8cf6210fd26405d2fb730154
NItin-36703 <nitin616pal@gmail.com>
2025-09-08 18:06:52 +0000

    date

---
+++ 01_basics/dates.js
@@ -0,0 +1,16 @@
+let mydate= new Date()
+console.log(mydate.toString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toDateString());
+console.log(typeof mydate)
+
+
+
+let cdate= new Date(2025,0,11)
+console.log(cdate.toDateString());
+
+
+let mytime = Date.now()
+console.log(mytime);
+
+
