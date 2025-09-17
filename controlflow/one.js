2b9d4b50f67eab9322399672f2215af3bfc26411
NItin-36703 <nitin616pal@gmail.com>
2025-09-17 18:00:07 +0000

    controlflow

--- controlflow/one.js
+++ controlflow/one.js
@@ -10,5 +10,16 @@ const userloggedin = true
 const debitcard=true
 
 if (userloggedin && debitcard ){
-    console.log("aloow the user to login ")
+    console.log("allow the user to login ")
 }
+
+// nullish 
+let val1;
+val1=5 ?? 10
+console.log(val1);
+
+//ternary operator 
+//condition ? true : false
+const salary=2000;
+salary >= 100 ?console.log("correct salary"):console.log("wrong salary");
+
